8ec3c1e5907c44d622f6efd2fb5bb4ee34d42ab6
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-16 16:34:49 -0400

    Trying out some options for the travis vm

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,7 @@
 # Hacking the build into Travis-CI "C" environment
 # based on http://blog.mlin.net/2013/02/testing-ocaml-projects-on-travis-ci.html
-# but using binary packages from http://opam.ocamlpro.com/doc/Quick_Install.html
+# but tried using binary packages from http://opam.ocamlpro.com/doc/Quick_Install.html
+# they seem to be down at the moment and maybe we only need ocaml-findlib that debian does have...
 
 export OPAM_PACKAGES='ocamlfind'
 
@@ -8,17 +9,19 @@ export OPAM_PACKAGES='ocamlfind'
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml
 
-# Install opam
-echo "deb http://www.recoil.org/~avsm/ wheezy main" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install opam
+# Install ocaml-findlib
+sudo apt-get install -qq ocaml-findlib
 
-opam init --auto-setup
-eval `opam config -env`
-popd
+# Install opam
+#echo "deb http://www.recoil.org/~avsm/ wheezy main" | sudo tee -a /etc/apt/sources.list
+#sudo apt-get update
+#sudo apt-get install opam
+#opam init --auto-setup
+#eval `opam config -env`
+#popd
 
 # Install any ocaml packages
-opam install "${OPAM_PACKAGES}"
+#opam install "${OPAM_PACKAGES}"
 
 # Install freetds
 sudo apt-get install freetds
