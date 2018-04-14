1196ee8ce31719ca20deabf19f49f8cfa1ea11a6
Brendan Long <self@brendanlong.com>
2018-04-14 14:12:52 -0400

    Use Travis CI Docker build
    Stole this from:
    
    https://github.com/andreas/ocaml-graphql-server/blob/master/.travis.yml

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,20 +1,13 @@
-# Hacking the build into Travis-CI "C" environment
-# based on http://blog.mlin.net/2013/02/testing-ocaml-projects-on-travis-ci.html
-# but tried using binary packages from http://opam.ocamlpro.com/doc/Quick_Install.html
-# they seem to be down at the moment and maybe we only need ocaml-findlib that debian does have...
-export OPAM_PACKAGES='jbuilder'
-
 if [ $TRAVIS_OS_NAME = osx ]; then
     brew unlink python
     brew install ocaml opam
+    opam init --auto-setup
+    eval `opam config env`
+    opam pin add -yn "$PACKAGE" .
+    opam install -y depext
+    opam depext -y "$PACKAGE"
+    opam install --deps-only -y "$PACKAGE"
+    opam install -y "$EXTRA_DEPS"
+else
+    wget https://raw.githubusercontent.com/ocaml/ocaml-travisci-skeleton/master/.travis-docker.sh
 fi
-
-# Setup opam
-opam init --auto-setup
-eval `opam config env`
-
-# Install OCaml dependencies
-opam pin add -yn freetds .
-opam install -y depext
-opam depext -y freetds
-opam install --deps-only -y freetds
