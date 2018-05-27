853a08cca5ff679100b648ac5cbe052c18fd8510
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2018-05-27 15:23:25 +0200

    Re-indent comments in Dblib interface

--- src/dblib.mli
+++ src/dblib.mli
@@ -59,52 +59,53 @@ val connect : ?user:string -> ?password:string ->
     files" is returned in {!Error}. *)
 
 val close : dbprocess -> unit
-    (** [close conn] close the connection [conn] to the server. *)
+(** [close conn] close the connection [conn] to the server. *)
 
 val use : dbprocess -> string -> unit
-    (** [use conn name] change the current database to [name].
-        @raise Dblib.Error if the database cannot be used. *)
+(** [use conn name] change the current database to [name].
+    @raise Dblib.Error if the database cannot be used. *)
 
 val name : dbprocess -> string
-    (** [name conn] returns the name of the current database. *)
+(** [name conn] returns the name of the current database. *)
 
 (************************************************************************)
 (** {2 Executing SQL queries and getting the results} *)
 
 val sqlexec : dbprocess -> string -> unit
-  (** Send the SQL command to the server and wait for an answer.
-      @raise Dblib.Error if the SQL query is incorrect or another problem
-      occurs.
+(** Send the SQL command to the server and wait for an answer.
+   @raise Dblib.Error if the SQL query is incorrect or another problem
+   occurs.
 
-      {b Warning}: There is one absolutely crucial, inflexible, unalterable
-      requirement: the application must process all rows produced by the
-      query. Before the [dbprocess] can be used for another query, the
-      application must either fetch all rows, or cancel the results and
-      receive an acknowledgement from the server. *)
+   {b Warning}: There is one absolutely crucial, inflexible,
+   unalterable requirement: the application must process all rows
+   produced by the query. Before the [dbprocess] can be used for
+   another query, the application must either fetch all rows, or
+   cancel the results and receive an acknowledgement from the
+   server. *)
 
 val cancel :  dbprocess -> unit
-    (** Cancel the current command batch.  *)
+(** Cancel the current command batch.  *)
 
 val canquery :  dbprocess -> unit
-    (** Cancel the query currently being retrieved, (retriving and)
-        discarding all pending rows. *)
+(** Cancel the query currently being retrieved, (retriving and)
+   discarding all pending rows. *)
 
 val results : dbprocess -> bool
-    (** [results conn] returns [true] if some results are available
-        and [false] if the query produced no results.  There may be
-        several results if COMPUTE clauses are used.
-        One MUST CALL this function before trying to retrieve any rows.
+(** [results conn] returns [true] if some results are available and
+   [false] if the query produced no results.  There may be several
+   results if COMPUTE clauses are used.  One MUST CALL this function
+   before trying to retrieve any rows.
 
-        @raise Dblib.Error if the query was not processed successfully by the
-        server. *)
+   @raise Dblib.Error if the query was not processed successfully by
+   the server. *)
 
 val numcols : dbprocess -> int
-    (** Return number of regular columns in a result set.  *)
+(** Return number of regular columns in a result set.  *)
 
 val colname : dbprocess -> int -> string
-    (** [colname conn c] returns the name of a regular result
-        column [c].  The first column has number 1.
-        @raise Invalid_argument if the column is not in range.  *)
+(** [colname conn c] returns the name of a regular result column [c].
+   The first column has number 1.
+   @raise Invalid_argument if the column is not in range.  *)
 
 type col_type =
   | SYBCHAR | SYBVARCHAR
@@ -121,11 +122,11 @@ type col_type =
   | SYBBINARY | SYBVARBINARY
 
 val string_of_col_type : col_type -> string
-  (** Returns a string description of the column type. *)
+(** Returns a string description of the column type. *)
 
 val coltype : dbprocess -> int -> col_type
-  (** Get the datatype of a regular result set column.
-     @raise Invalid_argument if the column does not exists. *)
+(** Get the datatype of a regular result set column.
+   @raise Invalid_argument if the column does not exists. *)
 
 type data =
   | NULL
@@ -147,14 +148,14 @@ type data =
 val string_of_data : data -> string
 
 val nextrow : dbprocess -> data list
-    (** Retrieve the next row.
-        @raise Not_found if no more rows are available. *)
+(** Retrieve the next row.
+   @raise Not_found if no more rows are available. *)
 
 val count : dbprocess -> int
-    (** Get count of rows processed.
-        - for insert/update/delete, count of rows affected.
-        - for select, count of rows returned, after all rows have been
-          fetched.    *)
+(** Get count of rows processed.
+    - for insert/update/delete, count of rows affected.
+    - for select, count of rows returned, after all rows have been
+      fetched.    *)
 
 (************************************************************************)
 (** {2 Error handling} *)
